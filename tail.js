835d39d317b6b407fad463ef783d27a9f6da848c
Denny Doan <denny-333@hotmail.com>
2023-01-09 18:01:21 -0500

    Added tail.js

--- tail.js
+++ tail.js
@@ -6,6 +6,17 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+//write a function
+const tail = (words) =>{
+  if (words.length < 1) {
+    return [];
+  }
+  console.log(words.slice(1));
+  // return array using slice method
+  return words.slice(1);
+};
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+const results = tail(words); // no need to capture the return value since we are not checking it
+assertEqual(results.length, 2); // original array should still have 3 elements!
+assertEqual(results[0],"Lighthouse");
+assertEqual(results[1],"Labs");
