f69b4e2d23ddbd89c0d1e40825ce56ae8d37d460
Denny Doan <denny-333@hotmail.com>
2023-01-09 17:37:19 -0500

    Lint check

--- tail.js
+++ tail.js
@@ -6,13 +6,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const head = (array) => {
-  if (array.length === 0) {
-    return undefined;
-  } else {
-    return array[0];
-  }
-};
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
