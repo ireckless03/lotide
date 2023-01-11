c0c2c8ff5a27484f5a1ad7acbd1c285a2b457bf1
Denny Doan <denny-333@hotmail.com>
2023-01-11 15:42:58 -0500

    Updated middle

--- middle.js
+++ middle.js
@@ -29,4 +29,8 @@ const middle = function(array) {
   }
 };
 
-
+middle([1]) // []
+middle([11,43]) // []
+middle([11,64,35]) // [64]
+middle([11,43,35,52,64,]) // [35]
+middle([11,43,35,52,64,78]) // [35, 52]
