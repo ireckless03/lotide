bb7f3f7d17e13d90e32e1ab0447056ecf71bd304
Denny Doan <denny-333@hotmail.com>
2023-01-13 09:44:20 -0500

    Eslint takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -25,24 +25,23 @@ const takeUntil = (array, callBack) => {
 };
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
 
 // console.log(results1);
 // console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
+
 // console.log(results2);
-console.log("Test 1")
-assertArraysEqual(takeUntil(data1, x => x < 0),[1, 2, 5, 7, 2] )
-assertArraysEqual(takeUntil(data2,x => x === ','),["I've", "been", "to", "Hollywood"] )
-console.log("------------------------------------")
+console.log("Test 1");
+assertArraysEqual(takeUntil(data1, x => x < 0),[1, 2, 5, 7, 2]);
+assertArraysEqual(takeUntil(data2,x => x === ','),["I've", "been", "to", "Hollywood"]);
+console.log("------------------------------------");
 
-console.log("Test 2")
-assertArraysEqual(takeUntil(data1, x => x < 6),[1, 2, 5] )
-assertArraysEqual(takeUntil(data2,x => x === 'been'),["I've"] )
-console.log("------------------------------------")
+console.log("Test 2");
+assertArraysEqual(takeUntil(data1, x => x < 6),[1, 2, 5]);
+assertArraysEqual(takeUntil(data2,x => x === 'been'),["I've"]);
+console.log("------------------------------------");
 
-console.log("Test 3")
-assertArraysEqual(takeUntil(data1, x => x === 4),[1, 2, 5, 7, 2, -1, 2] )
-assertArraysEqual(takeUntil(data2,x => x === 'Redwood'),["I've", "been", "to", "Hollywood", ",", "I've", "been", "to"] )
+console.log("Test 3");
+assertArraysEqual(takeUntil(data1, x => x === 4),[1, 2, 5, 7, 2, -1, 2]);
+assertArraysEqual(takeUntil(data2,x => x === 'Redwood'),["I've", "been", "to", "Hollywood", ",", "I've", "been", "to"]);
