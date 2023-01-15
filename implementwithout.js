3e23f224d2e602d602be824195f2eabf7059e0d1
Denny Doan <denny-333@hotmail.com>
2023-01-15 16:32:52 -0500

    Removed other verison of code and factored verison from mentor

--- implementwithout.js
+++ implementwithout.js
@@ -15,37 +15,18 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-
-// const without = function(source, itemsToRemove) {
-//   const result = source;
-//   for (let i = 0; i < result.length; i++) {
-//     for (let j = 0; j < itemsToRemove.length; j++) {
-//       if (source[i] === itemsToRemove[j]) {
-//         result.splice(i, 1);
-//       }
-//     }
-//   }
-//   return result;
-// };
-
-const without = function(original, itemsToRemove) {   
-  let withoutArray = [];   
-  for (i = 0; i < original.length; i++) 
-  {  if (itemsToRemove.includes(original[i]) === false) {       
-      withoutArray.push(original[i]);     
-    }   
-  }   
-  return withoutArray; 
+const without = function(original, itemsToRemove) {
+  let withoutArray = [];
+  for (i = 0; i < original.length; i++) {
+    if (itemsToRemove.includes(original[i]) === false) {
+      withoutArray.push(original[i]);
+    }
+  }
+  return withoutArray;
 };
 
 const words = ["hello", "world", "lighthouse"];
 without(words, ["hello"]);
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
-// middleground solution
-
-
-
-// // factored
-//  const without = (source, itemsToRemove) => source.filter(item => !itemsToRemove.includes(item));
 
