d46842c46782877626d7fe4dadb4421d51b31d2d
Denny Doan <denny-333@hotmail.com>
2023-01-16 19:58:12 -0500

    Update assertEqualTest

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,7 +1,7 @@
 // TEST CODE
-const  assertEqual = require('../assertEqual')
+const  assertEqual = require('../assertEqual');
 
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual(1, 1);
-assertEqual(4, 4)
+assertEqual(4, 4);
 
