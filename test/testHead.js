1e78d7fadd21ca556f1d5188497b5c7555bcc6ac
Denny Doan <denny-333@hotmail.com>
2023-01-16 19:39:26 -0500

    Added testing parameters for head.js

---
+++ test/testHead.js
@@ -0,0 +1,8 @@
+// TEST CODE
+const head = require('../head')
+const assertEqual = require('../assertEqual')
+
+assertEqual(head([1,6,7]), 1);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Long", "Days", "Ahead"]), "Long");
+
