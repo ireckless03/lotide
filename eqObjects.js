7dd414990ac46d39c54cfa13784b9627b0a7ff2c
Denny Doan <denny-333@hotmail.com>
2023-01-17 01:44:29 -0500

    eqOb passing tests

--- eqObjects.js
+++ eqObjects.js
@@ -1,6 +1,10 @@
 const assertEqual = require('./assertEqual')
 const eqArrays = require('./eqArrays')
 
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+
+
 // do they have same number of keys?
 // the value of each key is same as the other?
 const eqObjects = function(object1, object2) {
@@ -8,21 +12,18 @@ const eqObjects = function(object1, object2) {
   const objKeys2 = Object.keys(object2).sort();
   let objValue1 = [];
   let objValue2 = [];
-  console.log('objkeys',objKeys1);
-  console.log('objkeys',objKeys2);
 
   // make sure same number of keys
   if (objKeys1.length !== objKeys2.length) {
+    console.log('fail1')
     return false
   }
-  // check if keys are the same
-  console.log('keys for both',objKeys1,objKeys2)
-  eqArrays(objKeys1, objKeys2)
+  // check if keys are the same working
+  if (!eqArrays(objKeys1, objKeys2)) {
+    console.log('fail2')
+    return false
+  } 
 
-  for (let key of objKeys1) {
-    if (objKeys1[key] !== objKeys2[key])
-    console.log('objKey1[name')
-  }
   // make sure keys are the same
   // placing each keys value in an array
   for (key in object1) {
@@ -33,34 +34,19 @@ const eqObjects = function(object1, object2) {
     objValue2.push(object2[key])
   }
   // values of keys are showing crrectly
-  objValue1.sort()
-  objValue2.sort()
-  eqArrays(objValue1, objValue2)
-  // for (key in object1) {
-  //   //each key showing colour and size
-  //   objArr1.push(key)
-  //   console.log(key)
-  //   keys1++
-  // }
+  const sorted1 = objValue1.flatMap(num => num).sort()
+  const sorted2 = objValue2.flatMap(num => num).sort()
+  console.log(sorted1)
+  console.log(sorted2)
 
-  // object2.forEach(function(element, index) { 
-  //   console.log('element:',element,'index',index)
-  // })
-
-  // if true check if keys are the same
-  // looping thru the object keys, check the value of each key and make sure its the same value
-  // if so return true
-  // if not exit and return false
+  // objValue1.flatMap(x => x);
+  // objValue2.flatMap(x => x)
+  console.log('value 1',sorted1, ' val2',sorted2)
+  console.log('pass',eqArrays(sorted1, sorted2))
 };
 
+eqObjects(multiColorShirtObject, anotherMultiColorShirtObject)
+module.exports = eqObjects
 
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject= { size: "medium", color: "red" };
-eqObjects(shirtObject , anotherShirtObject); // => true
-//assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
-
-//const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
-//eqObjects(shirtObject , longSleeveShirtObject); // => false
-//assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
 
 
