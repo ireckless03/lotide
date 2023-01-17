ff5e53f4c6d86e8f9e68c264ad3f60485dfbf555
Denny Doan <denny-333@hotmail.com>
2023-01-17 01:53:02 -0500

    eqObjects linted

--- eqObjects.js
+++ eqObjects.js
@@ -1,8 +1,7 @@
-const assertEqual = require('./assertEqual')
-const eqArrays = require('./eqArrays')
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
 
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+
 
 
 // do they have same number of keys?
@@ -15,38 +14,42 @@ const eqObjects = function(object1, object2) {
 
   // make sure same number of keys
   if (objKeys1.length !== objKeys2.length) {
-    console.log('fail1')
-    return false
+    return false;
   }
   // check if keys are the same working
   if (!eqArrays(objKeys1, objKeys2)) {
-    console.log('fail2')
-    return false
-  } 
+    return false;
+  }
 
   // make sure keys are the same
   // placing each keys value in an array
   for (key in object1) {
-    objValue1.push(object1[key])
+    objValue1.push(object1[key]);
   }
 
   for (key in object2) {
-    objValue2.push(object2[key])
+    objValue2.push(object2[key]);
   }
   // values of keys are showing crrectly
-  const sorted1 = objValue1.flatMap(num => num).sort()
-  const sorted2 = objValue2.flatMap(num => num).sort()
-  console.log(sorted1)
-  console.log(sorted2)
+  const sorted1 = objValue1.flatMap(num => num).sort();
+  const sorted2 = objValue2.flatMap(num => num).sort();
+  console.log(sorted1);
+  console.log(sorted2);
 
   // objValue1.flatMap(x => x);
   // objValue2.flatMap(x => x)
-  console.log('value 1',sorted1, ' val2',sorted2)
-  console.log('pass',eqArrays(sorted1, sorted2))
+  console.log('value 1',sorted1, ' val2',sorted2);
+  console.log('pass',eqArrays(sorted1, sorted2));
 };
 
-eqObjects(multiColorShirtObject, anotherMultiColorShirtObject)
-module.exports = eqObjects
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject); // => true
+
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject); // => false
+
+module.exports = eqObjects;
 
 
 
