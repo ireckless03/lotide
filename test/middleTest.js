93e5485eee1cd1fd6556b065f146221a62ffc92d
Denny Doan <denny-333@hotmail.com>
2023-01-16 21:53:00 -0500

    Added MiddleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,27 @@
 //test code
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-middle([1]); // []
-middle([11,43]); // []
-middle([11,64,35]); // [64]
-middle([11,43,35,52,64,]); // [35]
-middle([11,43,35,52,64,78]);// [35, 52]
+
+it("returns [] for [1]", () => {
+  assert.deepEqual(middle([1]), []);
+});
+
+it("returns [] for [1, 2]", () => {
+  assert.deepEqual(middle([1, 2]), []);
+});
+
+it("returns [2] for [1, 2, 3]", () => {
+  assert.deepEqual(middle([1, 2, 3]), [2]);
+});
+
+it("returns [22, 13] for [1, 22, 13, 4]", () => {
+  assert.deepEqual(middle([1, 22, 13, 4]), [22, 13]);
+});
+
+it("3. returns ['Like','Sour','Candy'] for ['I', 'Like', 'Sour', 'Candy']", () => { assert.deepEqual(middle(['I', 'Like', 'Sour', 'Candy']), ['Like',"Sour"]); 
+});
+
+it("returns [22, 13] for [1, 22, 13, 4]", () => {
+  assert.deepEqual(middle([1, 22, 13, 4, 123, 42]), [13, 4]);
+});
