3407168d35c2a8101907870dce4e69d12f15b3eb
Denny Doan <denny-333@hotmail.com>
2023-01-16 19:37:12 -0500

    Turned head modular

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual')
 
 const head = (array) => {
   if (array.length === 0) {
@@ -14,5 +8,4 @@ const head = (array) => {
   }
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
